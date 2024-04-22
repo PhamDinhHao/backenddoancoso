c4fb72cc2e6003af1e138ccf8fac04b40831b62f
nkhoaa <ngokhoa0611@gmail.com>
2024-04-22 21:24:15 +0700

    add latest product api

--- src/route/web.js
+++ src/route/web.js
@@ -1,44 +1,57 @@
 import express from "express";
-import homeController from "../controllers/homeControlller"
-import userController from "../controllers/userController"
-import supplierController from "../controllers/supplierController"
-import customerController from "../controllers/customerController"
+import homeController from "../controllers/homeControlller";
+import userController from "../controllers/userController";
+import supplierController from "../controllers/supplierController";
+import customerController from "../controllers/customerController";
+import productController from "../controllers/productController";
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
+  router.get("/", homeController.getHomepage);
+  router.get("/crud", homeController.getCRUD);
+  router.post("/post-crud", homeController.postCRUD);
+  router.get("/hao", (req, res) => {
+    return res.send("Hello World ban Hao");
+  });
+  router.get("/get-crud", homeController.displayGetCRUD);
+  router.get("/edit-crud", homeController.getEditCRUD);
+  router.post("/put-crud", homeController.putCRUD);
+  router.get("/delete-crud", homeController.deleteCRUD);
+  router.post("/api/login", userController.handleLogin);
+  router.get("/api/get-all-users", userController.handleGetAllUsers);
+  router.post("/api/create-new-user", userController.handleCreateNewUser);
+  router.put("/api/edit-user", userController.handleEditUser);
+  router.delete("/api/delete-user", userController.handleDeleteUser);
 
-    router.get('/', homeController.getHomepage);
-    router.get('/crud', homeController.getCRUD);
-    router.post('/post-crud', homeController.postCRUD);
-    router.get('/hao', (req, res) => {
-        return res.send('Hello World ban Hao');
+  router.get("/api/get-all-supplier", supplierController.handleGetAllSupplier);
+  router.post(
+    "/api/create-new-supplier",
+    supplierController.handleCreateNewSupplier
+  );
+  router.put("/api/edit-supplier", supplierController.handleEditSupplier);
+  router.delete(
+    "/api/delete-supplier",
+    supplierController.handleDeleteSupplier
+  );
 
-    });
-    router.get('/get-crud', homeController.displayGetCRUD);
-    router.get('/edit-crud', homeController.getEditCRUD);
-    router.post('/put-crud', homeController.putCRUD);
-    router.get('/delete-crud', homeController.deleteCRUD);
-    router.post('/api/login', userController.handleLogin);
-    router.get('/api/get-all-users', userController.handleGetAllUsers);
-    router.post('/api/create-new-user', userController.handleCreateNewUser);
-    router.put('/api/edit-user', userController.handleEditUser);
-    router.delete('/api/delete-user', userController.handleDeleteUser);
+  router.get("/api/get-all-customer", customerController.handleGetAllCustomer);
+  router.post(
+    "/api/create-new-customer",
+    customerController.handleCreateNewCustomer
+  );
+  router.put("/api/edit-customer", customerController.handleEditCustomer);
+  router.delete(
+    "/api/delete-customer",
+    customerController.handleDeleteCustomer
+  );
 
-    router.get('/api/get-all-supplier', supplierController.handleGetAllSupplier);
-    router.post('/api/create-new-supplier', supplierController.handleCreateNewSupplier);
-    router.put('/api/edit-supplier', supplierController.handleEditSupplier);
-    router.delete('/api/delete-supplier', supplierController.handleDeleteSupplier);
+  router.get("/api/get-all-product", productController.handleGetAllProduct);
+  // router.post('/api/create-new-customer', customerController.handleCreateNewCustomer);
+  // router.put('/api/edit-customer', customerController.handleEditCustomer);
+  // router.delete('/api/delete-customer', customerController.handleDeleteCustomer);
 
-    router.get('/api/get-all-customer', customerController.handleGetAllCustomer);
-    router.post('/api/create-new-customer', customerController.handleCreateNewCustomer);
-    router.put('/api/edit-customer', customerController.handleEditCustomer);
-    router.delete('/api/delete-customer', customerController.handleDeleteCustomer);
-
-
-
-    router.get('/api/allcode', userController.getAllCode);
-    return app.use("/", router);
-
-}
+  router.get("/api/allcode", userController.getAllCode);
+  return app.use("/", router);
+};
 module.exports = initWebRoutes;
